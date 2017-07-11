7b7fac308f6de1c7ccbf420e38aa1087e6e46b52
Alex <oleksandr.barabanov@gmail.com>
2017-07-11 13:55:33 +0300

    implemented data rendering as table.

--- public/app.jsx
+++ public/app.jsx
@@ -1,12 +1,95 @@
 class Application extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrProperties: []
+    };
+  }
+
+  //getData() {
+  componentDidMount() {
+
+    const json = fetch('/api/properties');
+    json
+      .then(data => data.json())
+      .then(arr => {
+        console.info(`Loaded ${arr.length} items.`);
+        this.setState({
+          arrProperties: arr
+        });
+      })
+      .catch(err => console.error(err));
+
+  }
+
+
   render() {
+
+    const rows = this.state.arrProperties
+      .map((value, index) =>
+        <tr key={index}>
+          <td>{value.owner}</td>
+          <td>
+            {value.address.line1}<br/>
+            {value.address.line2 && 
+              <div>
+                {value.address.line2}<br/>
+              </div>
+            }
+            {value.address.line3 && 
+              <div>
+                {value.address.line3}<br/>
+              </div>
+            }
+            {value.address.line4}<br/>
+            {value.address.postCode}
+            <br/>
+            {value.address.city}
+            <br/>
+            {value.address.country}
+          </td>
+          <td>{value.incomeGenerated}</td>
+        </tr>
+      );
+    
     return (
       <div className="App">
+
         <div className="App-header">
           <h2>UI goes here</h2>
         </div>
 
-        { /*<Autocomplete />*/ }
+        <table>
+
+          <caption>
+            Properties List
+          </caption>
+
+          <colgroup>
+            <col id="owner" />
+            <col id="address" />
+            <col id="income" />
+          </colgroup>
+          
+          <thead>
+            <tr>
+              <th scope="col">Owner</th>
+              <th scope="col">Address</th>
+              <th scope="col">Generated Income</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {rows}
+          </tbody>
+
+        </table>
+
+        { /*
+          <button onClick={this.getData.bind(this)}>Load data</button>
+
+        */}
 
       </div>
     );
@@ -15,7 +98,8 @@ class Application extends React.Component {
 }
 
 ReactDOM.render(
-    <Application />,
-    document.getElementById('root')
+  <Application />,
+  document.getElementById('root')
 );
+
 
