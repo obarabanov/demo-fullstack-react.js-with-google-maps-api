b7a169db213b5d56d554ef8383328998cc58f3a2
Alex <oleksandr.barabanov@gmail.com>
2017-07-12 18:13:35 +0300

    geocoding works now, for every address from data table.

--- public/app.jsx
+++ public/app.jsx
@@ -13,9 +13,38 @@ class Application extends React.Component {
       .then(data => data.json())
       .then(arr => {
         console.info(`Loaded ${arr.length} data items.`);
+
         this.setState({
           arrProperties: arr
         });
+
+        //  firing geocoding
+        arr.forEach((element, index) => {
+          //console.log(element);
+
+          geocodeAddress(element.address, (results, status) => {
+            
+            let newObj = Object.assign({}, element);
+            newObj.geocodingStatus = status;
+            
+            if (status === 'OK') {
+              let res = results[0].geometry.location; // object LatLng
+              console.log(`geocoding result: ${res}`);
+              newObj.coordinates = res;
+            } else {
+              const msg = 'Geocode was not successful for the following reason: ' + status;
+              //alert( msg );
+              console.error(msg);
+            }
+
+            arr[index] = newObj;
+            this.setState({
+              arrProperties: arr
+            });
+
+          });
+        });
+
       })
       .catch(err => console.error(err));
   }
@@ -26,35 +55,50 @@ class Application extends React.Component {
       .map((value, index) =>
         <tr key={index}>
           <td>{value.owner.split(/\s+/)
-                .map( function(word) { 
-                  //console.log( word );
-                  return word.charAt(0).toUpperCase() + word.substr(1);
-                })
-                .join(' ')
+            .map(function (word) {
+              //console.log( word );
+              return word.charAt(0).toUpperCase() + word.substr(1);
+            })
+            .join(' ')
           }</td>
           <td>
-            {value.address.line1}<br/>
-            {value.address.line2 && 
+            {value.address.line1}<br />
+            {value.address.line2 &&
               <div>
-                {value.address.line2}<br/>
+                {value.address.line2}<br />
               </div>
             }
-            {value.address.line3 && 
+            {value.address.line3 &&
               <div>
-                {value.address.line3}<br/>
+                {value.address.line3}<br />
               </div>
             }
-            {value.address.line4}<br/>
+            {value.address.line4}<br />
             {value.address.postCode}
-            <br/>
+            <br />
             {value.address.city}
-            <br/>
+            <br />
             {value.address.country}
           </td>
           <td>{parseFloat(value.incomeGenerated).toFixed(2)} £</td>
+
+          <td>
+            Geocoding status: {value.geocodingStatus}
+            <br />
+            {value.coordinates &&
+              <div>
+                {`${value.coordinates}`}
+                <br />
+                {`${value.coordinates.lat()}`}
+                <br />
+                <a target="_blank" href="/public/map.html">show on map</a>
+              </div>
+            }
+          </td>
+
         </tr>
       );
-    
+
     return (
       <div className="App">
 
@@ -72,13 +116,15 @@ class Application extends React.Component {
             <col id="owner" />
             <col id="address" />
             <col id="income" />
+            <col id="coordinates" />
           </colgroup>
-          
+
           <thead>
             <tr>
               <th scope="col">Owner</th>
               <th scope="col">Address</th>
               <th scope="col">Generated Income</th>
+              <th scope="col">Within service area?</th>
             </tr>
           </thead>
 
@@ -99,4 +145,27 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const geocoder = new google.maps.Geocoder();
 
+function geocodeAddress(objAddress, callback) {
+  let address = getFullAddress(objAddress);
+  console.log(`geocoding address: ${address}`);
+  geocoder.geocode(
+    {
+      'address': address,
+      'region': 'GB'
+    },
+    callback
+  );
+}
+
+function getFullAddress(objAddress) {
+  return `
+    ${objAddress.line1} 
+    ${!objAddress.line2 ? '' : objAddress.line2} 
+    ${!objAddress.line3 ? '' : objAddress.line3} 
+    ${objAddress.line4} 
+    ${objAddress.postCode} 
+    ${objAddress.city} 
+    ${objAddress.country}`
+}
